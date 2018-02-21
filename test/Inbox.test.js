cc1f7d5a5adde175758251730345a34b15b50f5c
bearski <santosh.sewlal@gmail.com>
2018-02-21 13:37:55 +0200

    added contract deployment to test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,18 +2,23 @@ const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')  // constructor to create instances of the web3 library
 const web3 = new Web3(ganache.provider())
+const { interface, bytecode } = require('../compile')
 
 let accounts
+let inbox
 
 beforeEach(async () => {
   //  get a list of all accounts
   accounts = await web3.eth.getAccounts()
 
   // use one of those account to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' })
 })
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(accounts)
+    console.log(inbox)
   })
 })
