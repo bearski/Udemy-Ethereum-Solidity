32fd610b69ba5f48d629d599e32f26a273c8d06b
bearski <santosh.sewlal@gmail.com>
2018-02-22 08:14:47 +0200

    created deploy script

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const Web3 = require('web3')
+const { interface, bytecode } = require('./compile')
+
+const provider = new HDWalletProvider(
+  'coffee help snack state credit nurse piano impose glue crawl equal eye', // account mnemonic
+  'https://rinkeby.infura.io/v3gxKB8rcIJvR5IzMWKd'                          // Test Ethereum Network (Rinkeby)
+)
+
+const web3 = new Web3(provider)
+
+const deploy = async () => {        // helper to use async-await syntax for deploy
+  const accounts = await web3.eth.getAccounts()
+
+  console.log('Attempting to deploy from account ', accounts[0])
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] })
+
+  console.log('Contact deployed to ', result.options.address)
+}
+deploy()
