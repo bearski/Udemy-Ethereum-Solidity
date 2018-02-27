7e85a5355f3f928b465b5d85859a8ff6af4e2ac6
bearski <santosh.sewlal@gmail.com>
2018-02-27 08:00:56 +0200

    deploy file

--- deploy.js
+++ deploy.js
@@ -12,12 +12,12 @@ const web3 = new Web3(provider)
 const deploy = async () => {        // helper to use async-await syntax for deploy
   const accounts = await web3.eth.getAccounts()
 
-  console.log('Attempting to deploy from account ', accounts[0])
+  console.log('Attempting to deploy from account ', accounts[0])  //Attempting to deploy from account  0xCB2B908B49bf55E9e7a700d83072C541918e8571
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ gas: '1000000', from: accounts[0] })
 
-  console.log('Contact deployed to ', result.options.address)
+  console.log('Contact deployed to ', result.options.address)  //Contact deployed to  0x007B4a6e5bbcC52B3bC863b33e87f4457c5C4147
 }
 deploy()
