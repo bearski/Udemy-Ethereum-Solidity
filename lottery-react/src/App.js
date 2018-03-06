dfc1d89b1e811d9f60880da4e01b4f1434bc5567
bearski <santosh.sewlal@gmail.com>
2018-03-06 11:07:03 +0200

    building interactive front-ends

---
+++ lottery-react/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+
+class App extends Component {
+  state = {
+    manager: '',
+    players: [],
+    balance: ''
+  }
+
+  async componentDidMount() {
+      const manager = await lottery.methods.manager().call();
+      const players = await lottery.methods.getPlayers().call();
+      const balance = await web3.eth.getBalance(lottery.options.address);
+
+      this.setState({ manager, players, balance });
+  }
+
+  render() {
+    console.log(this.state.manager)
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          This contract is managed by { this.state.manager }
+          There are currently { this.state.players.length } people entered
+          competing to win { web3.utils.fromWei(this.state.balance, 'ether') }
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
