001bf338eaf98aaf605cc5a0bae582fff9e405db
bearski <santosh.sewlal@gmail.com>
2018-03-19 11:52:47 +0200

    updated and re-deployed contract

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x453576f1832ec2F62F94106F59477913f677bEa3'
+  '0x053Ccf7A509BD2c7fafC0F15431588807D343a4D'
 );
 
 export default instance;
