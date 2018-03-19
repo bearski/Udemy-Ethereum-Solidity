1c23ffb2ec89fd92ee41b858229cc133188ba64d
bearski <santosh.sewlal@gmail.com>
2018-03-19 11:05:28 +0200

    added routing

--- kickstart/components/Header.js
+++ kickstart/components/Header.js
@@ -1,44 +1,22 @@
-// import React from 'react';
-// import { Menu } from 'semantic-ui-react';
-// import { Link } from '../routes';
-//
-// export default () => {
-//   return (
-//     <Menu style={{ marginTop: '10px' }}>
-//       <Link route="/">
-//         <a className="item">CrowdCoin</a>
-//       </Link>
-//
-//       <Menu.Menu position="right">
-//         <Link route="/">
-//           <a className="item">Campaigns</a>
-//         </Link>
-//
-//         <Link route="/campaigns/new">
-//           <a className="item">+</a>
-//         </Link>
-//       </Menu.Menu>
-//     </Menu>
-//   );
-// };
-
-
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes';
 
 export default () => {
   return (
     <Menu style={{ marginTop: '10px' }}>
-      <Menu.Item>
-        CrowdCoin
-      </Menu.Item>
+      <Link route="/">
+        <a className="item">CrowdCoin</a>
+      </Link>
+
       <Menu.Menu position="right">
-      <Menu.Item>
-      Campaigns
-      </Menu.Item>
-      <Menu.Item>
-        +
-      </Menu.Item>
+        <Link route="/">
+          <a className="item">Campaigns</a>
+        </Link>
+
+        <Link route="/campaigns/new">
+          <a className="item">+</a>
+        </Link>
       </Menu.Menu>
     </Menu>
   );
